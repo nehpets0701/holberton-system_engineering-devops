8d08ef468986104cc3766d6b739cece60a46bce3
Stephen <nehpets0701@gmail.com>
2021-05-18 20:16:20 -0400

    pep8

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
+"""gets top ten posts from subreddit"""
 import requests
 
 
 def top_ten(subreddit):
     """top ten posts"""
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    r = requests.get(url, headers={'User-Agent': ''}, params = {'limit': 10})
+    r = requests.get(url, headers={'User-Agent': ''}, params={'limit': 10})
 
     if r.status_code != 200:
         return (0)
 
     for post in r.json()['data']['children']:
             print(post['data']['title'])
-
