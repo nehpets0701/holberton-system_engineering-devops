666117feec4f4cfa6db7fe3b8b485e11d1b2f4ee
Stephen <nehpets0701@gmail.com>
2021-05-18 20:05:44 -0400

    sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,7 @@ import requests
 def number_of_subscribers(subreddit):
     """gets number of subs from subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    r = requests.get(url, headers={'User-Agent': ''}, allow_redirects = 'false')
+    r = requests.get(url, headers={'User-Agent': 'Python:sub.counter:v0.1 (by /u/01100100011011110111)'}, allow_redirects = 'false')
 
     if r.status_code != 200:
         return (0)
