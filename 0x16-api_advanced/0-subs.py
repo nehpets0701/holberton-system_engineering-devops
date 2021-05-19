7d92aa3b0ee0eee159d8168c002ed8aa05db1912
Stephen <nehpets0701@gmail.com>
2021-05-18 20:04:44 -0400

    redirects

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,9 +5,9 @@ import requests
 def number_of_subscribers(subreddit):
     """gets number of subs from subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    r = requests.get(url, headers={'User-Agent': ''})
+    r = requests.get(url, headers={'User-Agent': ''}, allow_redirects = 'false')
 
     if r.status_code != 200:
         return (0)
 
-    return (r.json()['data']['subscribers'])
+    return r.json()['data']['subscribers']
