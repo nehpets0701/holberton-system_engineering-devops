b0ae4c8bbeaaa30b33e6f642ed1a46bf2e578fb5
Stephen <nehpets0701@gmail.com>
2021-05-20 13:58:39 -0400

    debugging puppet script

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# fixes small misspelling in apache config
+# spelling error in config file
 exec { 'modify_file':
     command => "sudo sed -i 's/phpp/php/g' '/var/www/html/wp-settings.php'",
     path    => '/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
